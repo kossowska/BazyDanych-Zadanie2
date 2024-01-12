dc15281134b4409f57d698e8cecf5ea07b6c0133
kossowska <148376882+kossowska@users.noreply.github.com>
2024-01-12 23:09:40 +0100

    Add files via upload

---
+++ indexes.sql
@@ -0,0 +1,29 @@
+-- Indeks na tabeli Konta
+CREATE INDEX index_nr_rachunku ON Konta (NumerRachunku);
+
+-- Indeksy na tabeli U¿ytkownicy
+CREATE INDEX index_pesel ON U¿ytkownicy (Pesel);
+CREATE INDEX index_nr_dok_toz ON U¿ytkownicy (NumerDokTo¿);
+
+-- Indeks dla tabeli Historia_logowania
+CREATE INDEX index_status_logowania ON Historia_logowania (StatusLogowania);
+
+-- Indeksy dla tabeli Transakcje
+CREATE INDEX index_typ_transakcji ON Transakcje (TypTransakcji);
+CREATE INDEX index_data_czas_transakcji ON Transakcje (DataCzasTransakcji);
+
+-- Indeks dla tabeli Typ_Dokumentu_to¿samoœci
+CREATE INDEX index_dok_nazwa ON Typ_Dokumentu_to¿samoœci (Nazwa);
+
+-- Indeksy dla tabeli Waluty
+CREATE INDEX index_waluta_nazwa ON Waluty (Nazwa);
+CREATE INDEX index_waluta_symbol ON Waluty (Symbol);
+
+-- Indeks dla tabeli Typ_Konta
+CREATE INDEX index_konto_nazwa ON Typ_Konta (Nazwa);
+
+-- Indeks dla tabeli Typ_transakcji
+CREATE INDEX index_transakcja_nazwa ON Typ_transakcji (Nazwa);
+
+-- Indeks dla tabeli Status_logowania
+CREATE INDEX index_status_logowania_nazwa ON Status_logowania (Nazwa);
---
+++ procedure.sql
@@ -0,0 +1,38 @@
+CREATE PROCEDURE SumaWp³atWyp³at
+	@KwotaWyp³at MONEY,
+	@KwotaWp³at MONEY
+AS
+BEGIN
+    WITH tabelaWp³at AS (
+        SELECT
+            CONVERT(DATE, T.DataCzasTransakcji) AS Dzieñ,
+            H.AdresIP,
+            SUM(T.KwotaTransakcji) AS SumaWp³at
+        FROM Transakcje T
+        INNER JOIN Historia_logowania H ON T.LogowanieID = H.ID
+        WHERE T.TypTransakcji = '1'
+        GROUP BY CONVERT(DATE, T.DataCzasTransakcji), H.AdresIP
+    ),
+    tabelaWyp³at AS (
+        SELECT
+            CONVERT(DATE, T.DataCzasTransakcji) AS Dzieñ,
+            H.AdresIP,
+            SUM(T.KwotaTransakcji) AS SumaWyp³at
+        FROM Transakcje T
+        INNER JOIN Historia_logowania H ON T.LogowanieID = H.ID
+        WHERE T.TypTransakcji = '4'
+        GROUP BY CONVERT(DATE, T.DataCzasTransakcji), H.AdresIP
+    )
+
+    SELECT
+        COALESCE(W.Dzieñ, Wy.Dzieñ) AS Dzieñ,
+        COALESCE(W.AdresIP, Wy.AdresIP) AS AdresIP,
+        COALESCE(W.SumaWp³at, 0) AS SumaWp³at,
+        COALESCE(Wy.SumaWyp³at, 0) AS SumaWyp³at
+    FROM tabelaWp³at W
+    FULL JOIN tabelaWyp³at Wy ON W.AdresIP = Wy.AdresIP AND W.Dzieñ = Wy.Dzieñ
+    WHERE ABS(Wy.SumaWyp³at) >= @KwotaWyp³at OR ABS(W.SumaWp³at) >= @KwotaWp³at
+	ORDER BY Dzieñ, AdresIP ASC;
+END;
+
+EXEC SumaWp³atWyp³at @KwotaWyp³at = 4000, @KwotaWp³at = 3000;
